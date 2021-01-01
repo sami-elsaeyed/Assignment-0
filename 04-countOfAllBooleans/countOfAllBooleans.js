c221d343b0683cc93816a94cde2490b51609e8d8
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-01 04:12:17 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,10 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  int count =0;
+  for(int i =0;i<=arr.length();i++){
+    if(arr[i] instanceOf Boolean)
+      count++;
+  }
+  return count;
 }
 
 // Do not edit this line;
