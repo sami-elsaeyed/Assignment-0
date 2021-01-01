c3d33131f000c0998848058a9bc29480f79ccd2f
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-01 18:22:12 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,11 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  var count=0;
+  for(var i=0;i<=nums.kegth;i++){
+    if(nums[i]<target){
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
