a2b016f4c00b49277431af0b618b70df973983fb
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-01 00:25:09 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  title=title.charAt(0).toUpperCase().concat(title.substring(1))
+  for(var i =1;i <=title.length;i++){
+    if(title.charAt(i)==" "){
+  if(title.charAt(i+1)==title.charAt(i+1).toLowerCase()){
+    title=title.substring(0,i).concat(" "+title.charAt(i+1).toUpperCase().concat(title.substring(i+2)));
+  }
+  }
+  }
+return s;
 }
 
 // Do not edit this line;
