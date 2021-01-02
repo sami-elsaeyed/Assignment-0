9ac0b97f4c7d52307a4c73b57256e98360fd051d
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-02 12:07:21 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,6 @@
 function pairSum(nums, target) {
   for(var i=0;i<=nums.length;i++){
-    for(var k=0;k<=nums.length;k++){
+    for(var k=i+1;k<=nums.length;k++){
       if(target-nums[i]==nums[k])
         return true;
     }
