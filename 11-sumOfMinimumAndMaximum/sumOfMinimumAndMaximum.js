f00b5c40b569191886752738f6e97952acd6158e
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-01 19:04:32 -0500

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,13 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  var min=nums[0];
+  var max=nums[0];
+  for(var i=0; i<nums.length;i++){
+    if(nums[i]>nums[i+1])
+      min=nums[i+1];
+    else
+      max=nums[i+1];
+  }
+  return max+min;
 }
 
 // Do not edit this line;
