9d2b0c82253e992b1d625c629e08e324485c73a9
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-02 14:09:24 -0500

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,14 +4,14 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    let mid= nums.length/2;
+    var mid= Math.floor(nums.length/2);
     if(nums[mid]==target){
       return true;
     }
     if (nums.length==1 & nums[0] != target)
       return false;
     if(nums[mid]<target)
-      return this.binarySearch(nums.slice(mid+1),target);
+      return this.binarySearch(nums.slice(mid),target);
     if(nums[mid]>target)
       return this.binarySearch(nums.slice(0,mid),target);
   }
