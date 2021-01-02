ff6c067ded0922abdd496d57f6586465ef80fdba
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-02 03:14:36 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -2,7 +2,7 @@ function isPalindrome(word) {
   if(word.length<=3 && word.charAt(0)==word.charAt(word.length-1))
     return true;
   if(word.charAt(0)==word.charAt(word.length))
-    return isPalindrome(word.substring(1,word.length-2))
+    return isPalindrome(word.substring(1,word.length-1))
   else 
     return false;
     
