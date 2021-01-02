2571d45811b087b05774ec1913ef9d95ad8cbeb6
sami-elsaeyed <66184138+sami-elsaeyed@users.noreply.github.com>
2021-01-02 14:52:06 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,12 @@
 function frequencyCounter(word) {
-  // Insert code here;
+ var freq=new Object();
+  for(var i=0;i<=word.length;i++){
+    if(freq.word.charAt(i)==null)
+      freq.word.charAt(i)=1;
+    else
+      freq.word.charAt(i)++;
+    return freq;
+  }
 }
 
 // Do not edit this line;
